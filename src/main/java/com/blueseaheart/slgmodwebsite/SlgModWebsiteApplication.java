a60f9794522ecdeb4238d3b29e58691b72e9a160
海蓝之心 <qw18081122506@163.com>
2025-07-25 13:42:56 +0800

    初始化spring boot项目

---
+++ src/main/java/com/blueseaheart/slgmodwebsite/SlgModWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.blueseaheart.slgmodwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlgModWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlgModWebsiteApplication.class, args);
+    }
+
+}
